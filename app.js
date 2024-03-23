4c3129c7e928e21b0ffb920bfdd2e7bd1e6acb36
safia-test <elmeslilis@gmal.com>
2024-03-23 08:30:22 +0000

    correction

--- app.js
+++ app.js
@@ -6,24 +6,14 @@ app.get('/',(req, res)=>{
     res.send('hello world!')
 })
 
-app.listen(port,()=>{
-    console.log(`example app listening on port ${port}`)
-})
 
 app.post('/a',(req, res)=>{
     res.send('got a post request ')
 })
 
 
-app.listen(port,()=>{
-    console.log(`example app listening on port ${port}`)
-})
-
 app.put('/user',(req, res)=>{
     res.send('got a put request at /user')
-})
-app.listen(port,()=>{
-    console.log(`example app listening on port ${port}`)
 })
 
 app.delete('/',(req, res)=>{
